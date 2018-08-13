80664e01f33467d12fa1cb25bb5f7d565d49c192
Evan Slaton <evanslaton@gmail.com>
2018-08-13 13:55:44 -0700

    Calculate daily cookie total

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
 'use strict'
 
-// Generates a random number
-function randomNum(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
 var hoursOfOp = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var pike = {
@@ -14,7 +9,52 @@ var pike = {
   avgCookies: 6.3,
   customersPerHour: [],
   cookiesPerHour: [],
-  dailyCookies: undefined
+  dailyCookieTotal: 0
+}
+
+var seatac = {
+  location: 'SeaTac Airport',
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgCookies: 1.2,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  dailyCookieTotal: 0
+}
+
+var seattle = {
+  location: 'Seattle Center',
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgCookies: 3.7,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  dailyCookieTotal: 0
+}
+
+var capitol = {
+  location: 'Capitol Hill',
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgCookies: 2.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  dailyCookieTotal: 0
+}
+
+var alki = {
+  location: 'Alki',
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgCookies: 4.6,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  dailyCookieTotal: 0
+}
+
+// Generates a random number
+function randomNum(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
 // Calculates customers per hour
@@ -25,11 +65,18 @@ pike.calcCustomersPerHour = function() {
   console.log(this.customersPerHour);
 }
 
-
 // Calculates cookies per hour
 pike.calcCookiesPerHour = function() {
-  for (var i = 0; i < hoursOfOp.length; i++) {
+  for (var i = 0; i < this.customersPerHour.length; i++) {
     this.cookiesPerHour.push(Math.round(this.customersPerHour[i] * this.avgCookies));
   }
   console.log(this.cookiesPerHour);
+}
+
+// Calculates daily cookie total
+pike.calcDailyTotalCookies = function() {
+  for (var i = 0; i < this.cookiesPerHour.length; i++) {
+    this.dailyCookieTotal += this.cookiesPerHour[i];
+  }
+  console.log(this.dailyCookieTotal);
 }
